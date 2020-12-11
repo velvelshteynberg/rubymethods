b86c8cc616ffc5269d4a228ead97e553686ac387
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-10 22:30:10 -0500

    first commit

---
+++ excercise6.rb
@@ -0,0 +1,11 @@
+
+#excercise 6
+def converting_temperature (temperature_farenheight)
+    celcius = (temperature_farenheight-32.0)*(5.0/9)
+    return celcius
+end
+
+
+puts "What is the temperature in farenheight?"
+temperature_farenheight = gets.chomp.to_i
+puts converting_temperature (temperature_farenheight)
