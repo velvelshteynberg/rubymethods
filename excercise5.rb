3b4db8fdc14f6845092f08f7d3c79e3ed959a0d7
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-10 22:32:10 -0500

    first practice

---
+++ excercise5.rb
@@ -0,0 +1,9 @@
+#excercise 5
+def greet_backwards(first_name)
+    return "Hello, #{first_name.reverse}#{first_name.reverse}! Welcome home.!!!"
+end 
+
+puts greet_backwards('Bob')
+puts greet_backwards('Shirly')
+puts greet_backwards('Sue')
+puts greet_backwards('Andy')
