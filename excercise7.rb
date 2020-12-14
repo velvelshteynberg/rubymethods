63296412b037bdc83ffc9a09b8e4547d452d0a4c
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-14 08:15:24 -0500

    first practice

--- excercise7.rb
+++ excercise7.rb
@@ -3,13 +3,14 @@ def wrap_text(first, second)
     return "#{second}#{first}#{second}"
     
 end
+return_1 = wrap_text('hello', '===')
  def second_method(return_1)
     return "---===###new message ###===---"
  end
 
- return_1 = wrap_text('hello', '===')
+ 
 
-puts wrap_text('hello', '===')
+# puts wrap_text('hello', '===')
 
 # puts "---===####{wrap_text('new message', '')}###===---"
 
