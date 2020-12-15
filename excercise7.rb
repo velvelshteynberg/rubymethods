51de525098f2dc48579ae5e5f1a94101d72c3b86
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-14 21:25:27 -0500

    elvis teaching me about wrapping

--- excercise7.rb
+++ excercise7.rb
@@ -3,10 +3,7 @@ def wrap_text(first, second)
     return "#{second}#{first}#{second}"
     
 end
-return_1 = wrap_text('hello', '===')
- def second_method(return_1)
-    return "---===###new message ###===---"
- end
+
 
  
 
@@ -14,4 +11,4 @@ return_1 = wrap_text('hello', '===')
 
 # puts "---===####{wrap_text('new message', '')}###===---"
 
-puts second_method(return_1)
+puts wrap_text(wrap_text(wrap_text("Hello","###"), "==="),"---")
--- excercise8.rb
+++ excercise8.rb
@@ -1,11 +1,35 @@
-puts "How many minutes did it take you?"
-time = (gets.chomp.to_i)*60
-puts "How many metres did you run?"
-length_metres = gets.chomp.to_i
+def distance_and_speed(time,length)
+    return "You travelled at a speed of #{first}/#{second} metres/second"
 
-def distance_and_speed(first,second)
-    return "You travelled #{second} metres in #{first} seconds which is a speed of #{first}/#{second}"
-   end
+end
 
+   puts "How many minutes did it take you user 1?"
+   user1_time = (gets.chomp.to_i)*60
+   puts "How many metres did you run?"
+   user1_length_metres = gets.chomp.to_i
+   puts distance_and_speed('user1_time','user1_length_metres')
 
-puts distance_and_speed(time, length_metres)
+   puts "How many minutes did it take you user 2?"
+   user2_time = (gets.chomp.to_i)*60
+   puts "How many metres did you run?"
+   user2_length_metres = gets.chomp.to_i
+   puts distance_and_speed(user2_time, user2_length_metres)
+
+   puts "How many minutes did it take you user 3?"
+   user3_time = (gets.chomp.to_i)*60
+   puts "How many metres did you run?"
+   user3_length_metres = gets.chomp.to_i
+   puts distance_and_speed(user3_time, user3_length_metres)
+
+   if speed3 > speed2 && speed3 > speed1
+    puts "Person 3 was the fastest at #{speed3} m/s"
+  elsif speed2 > speed3 && speed2 > speed1
+    puts "Person 2 was the fastest at #{speed2} m/s"
+  elsif speed1 > speed3 && speed1 > speed2
+    puts "Person 1 was the fastest at #{speed1} m/s"
+  elsif speed1 == speed2 && speed2 == speed3
+    puts "Everyone tied at #{speed1} m/s"
+  else
+    puts "Well done everyone!"
+  end
+  
